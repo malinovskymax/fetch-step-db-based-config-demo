20f20db3a64bae217f7da8ff41a95d4bd9b5dc1e
Maxim Malinovsky <malinovsky.dev@gmail.com>
2018-07-16 19:21:43 +0300

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,6 +1,5 @@
- #!/bin/bash
+#!/bin/bash
 
- cd fetch-step-db-based-config-demo
- echo 'second step done' >> tempfile.txt
- cat tempfile.txt
- 
+cd fetch-step-db-based-config-demo
+echo 'second step done' >> tempfile.txt
+cat tempfile.txt
